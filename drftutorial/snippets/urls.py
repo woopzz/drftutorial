f1bab7a674ca2c961a773ae52f6b0d812e3b7a3f
woopzz <bkovtunovych@gmail.com>
2024-04-22 22:07:20 +0000

    Fix typo. Use UserViewSet for the user router.

--- drftutorial/snippets/urls.py
+++ drftutorial/snippets/urls.py
@@ -5,7 +5,7 @@ from drftutorial.snippets import views
 
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet, basename='snippet')
-router.register(r'users', views.SnippetViewSet, basename='user')
+router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
